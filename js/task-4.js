c9e07934dd24ac1a075405a65c6548ed05f5e712
Darialucky <144655337+Darialucky@users.noreply.github.com>
2023-12-21 20:26:38 +0200

    all2

--- js/task-4.js
+++ js/task-4.js
@@ -1,7 +1,7 @@
 const getTotalBalanceByGender = (users, gender) => {
     return users
     .filter(user => user.gender === gender)
-    .reduce((totalBalance, user) => totalBalance + user.balance, 0);
+    .reduce((balance, user) => balance + user.balance, 0);
 };
 
 const allUsers1 = [
